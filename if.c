612300788db19b572e68084795c8a1e813833f76
sowmiya-BME <ajithsowmi.4624@gmail.com>
2025-05-08 19:24:16 +0530

    Create if.c

---
+++ if.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+   int main(){
+       int number=25;
+       
+   
+       if (number>0){
+           printf("the number is possitive.\n");
+       }
+       else if(number<0){
+           printf("the number is negative.\n");
+       }
+       return 0;
+       }
+    
